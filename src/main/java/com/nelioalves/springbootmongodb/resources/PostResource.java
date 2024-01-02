e7d2c5a0ef185d247e7ad60a18237f4d116bba64
gabrielszg <gabrielsouza5@hotmail.com>
2024-01-02 13:49:45 -0300

    feat: simple query with query methods

--- src/main/java/com/nelioalves/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/nelioalves/springbootmongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.nelioalves.springbootmongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.springbootmongodb.domain.Post;
+import com.nelioalves.springbootmongodb.resources.utils.URL;
 import com.nelioalves.springbootmongodb.services.PostService;
 
 @RestController
@@ -21,6 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 	
 }
