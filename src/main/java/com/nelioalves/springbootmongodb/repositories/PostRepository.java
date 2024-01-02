10fb3316de41e03ccf19c0f0bd99d2f42f090e31
gabrielszg <gabrielsouza5@hotmail.com>
2024-01-02 13:57:35 -0300

    feat: simple query with @query

--- src/main/java/com/nelioalves/springbootmongodb/repositories/PostRepository.java
+++ src/main/java/com/nelioalves/springbootmongodb/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.nelioalves.springbootmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String texy);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
